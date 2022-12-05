4ac50a883cac3ff1f2e6ee8a6660dd8c850033ed
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2022-12-05 15:26:47 -0300

    Corrección

--- hooks/usePosts.ts
+++ hooks/usePosts.ts
@@ -1,17 +1,14 @@
-const usePosts = () =>{
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const data = await response.json();
-      setPosts(data);
-    };
-
-    fetchPosts();
-  }, []);
-
-  return posts;
+const usePosts = () => {
+  // const [posts, setPosts] = useState<Post[]>([]);
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  //     const data = await response.json();
+  //     setPosts(data);
+  //   };
+  //   fetchPosts();
+  // }, []);
+  // return posts;
 }
 
 export default usePosts
